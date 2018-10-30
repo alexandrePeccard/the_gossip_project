bf0fa9a41d8a2a1ad01cafc980f5150db0cf88b7
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-10-30 16:50:55 +0100

    add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+	#root url
   get '/', to: 'gossip#index'
+  #bienvenue
 	get '/bienvenue/:user', to: 'bienvenue#index'
-	get '/contact', to: 'contact#index'
-  get '/team', to: 'team#index'
-  get '/', to: 'root#index'
+	#user
+  get '/users', to: 'user#index'
+  get '/users/new', to: 'user#new', as: 'user_n'
+  #gossips
   get '/gossips', to: 'gossip#index'
   get '/gossips/:id/show', to: 'gossip#show', as: 'gossip_s'
   get '/gossips/:id/edit', to: 'gossip#edit', as: 'gossip_e'
+	#contact
+	get '/contact', to: 'contact#index'
+	#team
+  get '/team', to: 'team#index'
+
+  post '/users/create', to: 'user#create', as: 'user_c'
 end
