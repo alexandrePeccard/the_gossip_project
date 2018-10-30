04988aac5df61cbd2495dc087a2a1725e6ea7732
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-10-30 15:13:20 +0100

    fix root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get '/', to: 'gossip#index'
 	get '/bienvenue/:user', to: 'bienvenue#index'
 	get '/contact', to: 'contact#index'
   get '/team', to: 'team#index'
+  get '/', to: 'root#index'
   get '/gossips', to: 'gossip#index'
   get '/gossips/:id/show', to: 'gossip#show', as: 'gossip_s'
   get '/gossips/:id/edit', to: 'gossip#edit', as: 'gossip_e'
